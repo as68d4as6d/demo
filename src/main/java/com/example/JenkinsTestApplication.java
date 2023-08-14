ec98e7a7ef9800640cb0a3664e58050fdff1b28b
as68d4as6d <1907186615@qq.com>
2023-08-14 14:38:50 +0800

    测试2

--- src/main/java/com/example/JenkinsTestApplication.java
+++ src/main/java/com/example/JenkinsTestApplication.java
@@ -8,7 +8,7 @@ public class JenkinsTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run ( JenkinsTestApplication.class, args );
-        System.out.println ("-------1-------" +
+        System.out.println ("-------12-------" +
                 "-----------------------------------------");
     }
 
