f7dd0332225c986fced24083b6a9c2cf4d4aa200
as68d4as6d <1907186615@qq.com>
2023-08-14 10:09:17 +0800

    测试

---
+++ src/main/java/com/example/JenkinsTestApplication.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run ( JenkinsTestApplication.class, args );
+        System.out.println ("--------------" +
+                "-----------------------------------------");
+    }
+
+}
